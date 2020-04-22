2225e038d65b1395626468c001cddbee895c2dbc
call203 <call203@daum.net>
2020-04-23 00:03:55 +0900

    stepcount_sensor

--- app/src/main/java/org/walkpackage/notification/AccelerometerFragment.java
+++ app/src/main/java/org/walkpackage/notification/AccelerometerFragment.java
@@ -34,13 +34,18 @@ import static android.content.Context.SENSOR_SERVICE;
  */
 public class AccelerometerFragment extends Fragment implements SensorEventListener{
     private SensorManager mSensorManager;
-    private Sensor mAccelerometer;
-    TextView xValue, yValue, zValue;
+    private Sensor mAccelerometer, mStepCountSensor;
+    TextView xValue, yValue, zValue,stepCount;
     private Thread thread;
     private Button btnStart, btnStop, btnReset;
     private LineGraphSeries<DataPoint> mSeriesAccelX, mSeriesAccelY, mSeriesAccelZ;
     private GraphView mGraphAccel;
     private double graphLastAccelXValue = 10d;
+
+    //현재 걸음 수
+    private int mSteps = 0;
+//    //리스너가 등록되고 난 후의 step count
+//    private int mCounterSteps = 0;
 
     //For 파이어베이스
     FirebaseAuth firebaseAuth;
@@ -61,19 +66,28 @@ public class AccelerometerFragment extends Fragment implements SensorEventListen
         uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
         final View view = inflater.inflate(R.layout.fragment_accelerometer, container, false);
+        //가속도 센서 설정
         mSensorManager = (SensorManager) getActivity().getSystemService(SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        //pedometer센서 설정
+        mStepCountSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
 
-        mSeriesAccelX = initSeries(Color.BLUE, "X"); //라인 그래프를 그림
-        mSeriesAccelY = initSeries(Color.RED, "Y");
-        mSeriesAccelZ = initSeries(Color.GREEN, "Z");
+
+
         mGraphAccel = initGraph((GraphView) view.findViewById(R.id.graph), "X, Y, Z direction Acceleration");
+
 
 
 
         xValue = (TextView) view.findViewById(R.id.xValue);
         yValue = (TextView) view.findViewById(R.id.yValue);
         zValue = (TextView) view.findViewById(R.id.zValue);
+        stepCount = (TextView) view.findViewById(R.id.stepcount);
+
+        xValue.setText("xValue: " + 0.0);
+        yValue.setText("yValue: " + 0.0);
+        zValue.setText("zValue: " + 0.0);
+        stepCount.setText("걸음수: " + 0 +" 걸음");
 
         //버튼추가
         btnStart = (Button)view.findViewById(R.id.btnStart);
@@ -107,6 +121,10 @@ public class AccelerometerFragment extends Fragment implements SensorEventListen
 
     public void startAccel(){
 
+        mSeriesAccelX = initSeries(Color.BLUE, "X"); //라인 그래프를 그림
+        mSeriesAccelY = initSeries(Color.RED, "Y");
+        mSeriesAccelZ = initSeries(Color.GREEN, "Z");
+
         //그래프에 x,y,z 추가
         mGraphAccel.addSeries(mSeriesAccelX);
         mGraphAccel.addSeries(mSeriesAccelY);
@@ -114,30 +132,53 @@ public class AccelerometerFragment extends Fragment implements SensorEventListen
         if (mAccelerometer != null) {
             mSensorManager.registerListener((SensorEventListener) this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
         }
+
+        if (mStepCountSensor != null) {
+            //센서의 속도 설정
+            mSensorManager.registerListener(this, mStepCountSensor, SensorManager.SENSOR_DELAY_GAME);
+
+        }
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        float x, y, z;
-        x = event.values[0];
-        y = event.values[1];
-        z = event.values[2];
 
-        xValue.setText("xValue: " + x);
-        yValue.setText("yValue: " + y);
-        zValue.setText("zValue: " + z);
+        if (event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+            float x, y, z;
+            x = event.values[0];
+            y = event.values[1];
+            z = event.values[2];
 
-        //파이어베이스에 저장
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("RecordAccelerometer");
-        ref.child(uid).child("x").push().setValue(Double.toString(x));
-        ref.child(uid).child("y").push().setValue(Double.toString(y));
-        ref.child(uid).child("z").push().setValue(Double.toString(z));
+            xValue.setText("xValue: " + x);
+            yValue.setText("yValue: " + y);
+            zValue.setText("zValue: " + z);
 
-        graphLastAccelXValue += 0.05d;
+            //파이어베이스에 저장
+            DatabaseReference ref = FirebaseDatabase.getInstance().getReference("RecordAccelerometer");
+            ref.child(uid).child("x").push().setValue(Double.toString(x));
+            ref.child(uid).child("y").push().setValue(Double.toString(y));
+            ref.child(uid).child("z").push().setValue(Double.toString(z));
 
-        mSeriesAccelX.appendData(new DataPoint(graphLastAccelXValue,x),true,100);
-        mSeriesAccelY.appendData(new DataPoint(graphLastAccelXValue,y),true,100);
-        mSeriesAccelZ.appendData(new DataPoint(graphLastAccelXValue,z),true,100);
+            graphLastAccelXValue += 0.05d;
+
+            mSeriesAccelX.appendData(new DataPoint(graphLastAccelXValue, x), true, 100);
+            mSeriesAccelY.appendData(new DataPoint(graphLastAccelXValue, y), true, 100);
+            mSeriesAccelZ.appendData(new DataPoint(graphLastAccelXValue, z), true, 100);
+        }
+
+        //[걸음 수 센서]
+        if (event.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+
+            //stepcountsenersor는 앱이 꺼지더라도 초기화 되지않는다. 그러므로 우리는 초기값을 가지고 있어야한다.
+//            if (mCounterSteps < 1) {
+//                // initial value
+//                mCounterSteps = (int) event.values[0];
+//            }
+            //리셋 안된 값 + 현재값 - 리셋 안된 값
+            mSteps = (int) event.values[0];
+            stepCount.setText("걸음수: " + Integer.toString(mSteps)+" 걸음");
+
+        }
     }
 
     @Override
@@ -179,9 +220,9 @@ public class AccelerometerFragment extends Fragment implements SensorEventListen
     //리셋버튼
     public void ResetAccel(){
 
-        xValue.setText("xValue: " + 0);
-        yValue.setText("yValue: " + 0);
-        zValue.setText("zValue: " + 0);
+        xValue.setText("xValue: " + 0.0);
+        yValue.setText("yValue: " + 0.0);
+        zValue.setText("zValue: " + 0.0);
 
         mGraphAccel.removeAllSeries();
 
--- app/src/main/java/org/walkpackage/notification/MainActivity.java
+++ app/src/main/java/org/walkpackage/notification/MainActivity.java
@@ -2,6 +2,7 @@ package org.walkpackage.notification;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,19 +12,39 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.techtown.walkingresearch.R;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
   Button  mLoginBtn;
   TextView mResigettxt;
   EditText mEmailText, mPasswordText;
-  private FirebaseAuth firebaseAuth;
+  private FirebaseAuth firebaseAuth = null;
+
+  private GoogleSignInClient mGoogleSignInClient;
+  private static final int RC_SIGN_IN = 9001;
+  private SignInButton signInithGoogle_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +58,28 @@ public class MainActivity extends AppCompatActivity {
         mLoginBtn = findViewById(R.id.login_btn);
         mEmailText = findViewById(R.id.emailEt);
         mPasswordText = findViewById(R.id.passwordEdt);
+
+        //구글 Sign in
+        signInithGoogle_btn = findViewById(R.id.GoogleSignin_btn);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+//        if (firebaseAuth.getCurrentUser() != null) {
+//            Intent intent = new Intent(getApplication(), BottomNaviActivity.class);
+//            startActivity(intent);
+//            finish();
+//        }
+        signInithGoogle_btn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                signInWithGoogle();
+            }
+        });
+
 
 
         //가입 버튼이 눌리면
@@ -74,5 +117,85 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+    //구글로 로그인
+    private void signInWithGoogle() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            final int[] exist = {0};
+                            FirebaseDatabase database = FirebaseDatabase.getInstance();
+                            DatabaseReference reference = database.getReference("Users");
+
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            String email = user.getEmail();
+                            final String uid = user.getUid();
+                            String name = user.getDisplayName();
+
+                            //해쉬맵 테이블을 파이어베이스 데이터베이스에 저장
+
+                            reference.addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot dataSnapshot) {
+                                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+
+                                        if (snapshot.getKey().equals(uid)){
+                                            exist[0] = 1;
+                                        }
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                }
+                            });
+
+                            if(exist[0] == 0){
+                                //해쉬맵 테이블을 파이어베이스 데이터베이스에 저장
+                                Log.d("기록 완료!",uid);
+
+                                HashMap<Object,String> hashMap = new HashMap<>();
+
+                                hashMap.put("uid",uid);
+                                hashMap.put("email",email);
+                                hashMap.put("name",name);
+
+                                reference.child(uid).setValue(hashMap);
+
+                            }
+
+                            Intent intent = new Intent(MainActivity.this, BottomNaviActivity.class);
+                            startActivity(intent);
+
+                        } else {
+
+                        }
+                    }
+                });
     }
 }
